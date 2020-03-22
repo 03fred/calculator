928b9e101934b95ce66602d58e9159bbffea6128
Fred <frederico.d.silva@outlook.com>
2020-03-22 12:08:57 -0300

    hotfix undefined variable

--- App.js
+++ App.js
@@ -1,57 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react'
+import {Platform, StyleSheet, Text, View} from 'react-native'
+import Button from './src/components/Button'
+import Display from './src/components/Display'
 
-import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import Button from './src/components/Button';
-import Display from './src/components/Display';
-
-const initialSate = {
+const initialState = {
   displayValue: '0',
   clearDisplay: false,
   operation: null,
   values: [0, 0],
-  current: 0
+  current: 0,
 }
 
-
 export default class App extends Component {
-
-  state = { ...initialSate }
+  state = { ...initialState }
 
   addDigit = n => {
-    if (n === '.' && this.state.displayValue.includes('.')) {
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+    
+    if (n === '.' && !clearDisplay 
+      && this.state.displayValue.includes('.')) {
       return
     }
 
-    const clearDisplay = this.state.displayValue === '0'
-      || this.state.clearDisplay
-
     const currentValue = clearDisplay ? '' : this.state.displayValue
-
-    const displayValue = current + n
-
+    const displayValue = currentValue + n
     this.setState({ displayValue, clearDisplay: false })
 
     if (n !== '.') {
@@ -60,61 +33,64 @@ export default class App extends Component {
       values[this.state.current] = newValue
       this.setState({ values })
     }
-
   }
+
   clearMemory = () => {
-
-    this.setState({ ...initialSate })
+    this.setState({ ...initialState })
   }
+
   setOperation = operation => {
     if (this.state.current === 0) {
       this.setState({ operation, current: 1, clearDisplay: true })
     } else {
-      const equals = operation === '=';
+      const equals = operation === '='
       const values = [...this.state.values]
       try {
-        values[0] =
+        values[0] = 
           eval(`${values[0]} ${this.state.operation} ${values[1]}`)
       } catch (e) {
         values[0] = this.state.values[0]
       }
+
       values[1] = 0
       this.setState({
-        displayValue: values[0],
+        displayValue: `${values[0]}`,
         operation: equals ? null : operation,
         current: equals ? 0 : 1,
-        clearDisplay : !equals,
+        //clearDisplay: !equals,
+        clearDisplay: true,
         values,
       })
     }
   }
-}
-const App: () => React$Node = () => {
-  return (
-    <View style={styles.container}>
-      <Display value={this.state.displayValue} />
-      <View style={styles.button}>
-        <Button label='AC' triple onCLick={this.clearMemory} />
-        <Button label='/' operation onCLick={() => this.setOperation} />
-        <Button label='7' onCLick={addDigit} />
-        <Button label='8' onCLick={addDigit} />
-        <Button label='9' onCLick={addDigit} />
-        <Button label='*' operation onCLick={() => this.setOperation} />
-        <Button label='4' onCLick={addDigit} />
-        <Button label='5' onCLick={addDigit} />
-        <Button label='6' onCLick={addDigit} />
-        <Button label='-' operation={() => this.setOperation} />
-        <Button label='1' onCLick={addDigit} />
-        <Button label='2' onCLick={addDigit} />
-        <Button label='3' onCLick={addDigit} />
-        <Button label='+' operation onCLick={() => this.setOperation} />
-        <Button label='0' double onCLick={addDigit} />
-        <Button label='.' onCLick={() => this.addDigit} />
-        <Button label='=' operation onCLick={() => this.setOperation} />
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clearMemory} />
+          <Button label='/' operation onClick={this.setOperation} />
+          <Button label='7' onClick={this.addDigit} />
+          <Button label='8' onClick={this.addDigit} />
+          <Button label='9' onClick={this.addDigit} />
+          <Button label='*' operation  onClick={this.setOperation} />
+          <Button label='4' onClick={this.addDigit} />
+          <Button label='5' onClick={this.addDigit} />
+          <Button label='6' onClick={this.addDigit} />
+          <Button label='-' operation onClick={this.setOperation} />
+          <Button label='1' onClick={this.addDigit} />
+          <Button label='2' onClick={this.addDigit} />
+          <Button label='3' onClick={this.addDigit} />
+          <Button label='+' operation onClick={this.setOperation} />
+          <Button label='0' double  onClick={this.addDigit} />
+          <Button label='.' onClick={this.addDigit} />
+          <Button label='=' operation onClick={this.setOperation} />
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -125,5 +101,3 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   }
 });
-
-export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { StyleSheet, Text, Dimensions, TouchableHighLight } from 'react-native'
+import {
+    StyleSheet,
+    Text,
+    Dimensions,
+    TouchableHighlight
+} from 'react-native'
 
 const styles = StyleSheet.create({
-
     button: {
-
         fontSize: 40,
         height: Dimensions.get('window').width / 4,
         width: Dimensions.get('window').width / 4,
@@ -13,30 +16,27 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888',
-
     },
     operationButton: {
         color: '#fff',
-        backgroundColor: '##fa8B231'
+        backgroundColor: '#fa8231',
     },
-    buttonDoule: {
-        width: (Dimensions.get('window').width / 4) * 2
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
     },
-
     buttonTriple: {
-        width: (Dimensions.get('window').width / 4) * 3
-    },
+        width: (Dimensions.get('window').width / 4) * 3,
+    }
 })
 
 export default props => {
     const stylesButton = [styles.button]
-
-    if (props.double) stylesButton.push(styles.buttonDoule);
-    if (props.triple) stylesButton.push(styles.buttonTriple);
-    if (props.operation) stylesButton.push(styles.operationButton);
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operation) stylesButton.push(styles.operationButton)
     return (
-        <TouchableHighLight onPress={() => props.onClick(props.label)}>
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
             <Text style={stylesButton}>{props.label}</Text>
-        </TouchableHighLight>
+        </TouchableHighlight>
     )
 }
